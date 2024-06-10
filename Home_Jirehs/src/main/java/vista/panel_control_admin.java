16e266c98cebc581b961090df867df10b87aaf9c
Darien <62579599+lDarienl@users.noreply.github.com>
2024-06-10 13:51:10 -0500

    Actualizacion paquete
    Se actualiza cada vista con el "package vista;"

--- Home_Jirehs/src/main/java/vista/panel_control_admin.java
+++ Home_Jirehs/src/main/java/vista/panel_control_admin.java
@@ -2,11 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-
+package vista;
 /**
  *
  * @author pc
  */
+
 public class panel_control_admin extends javax.swing.JFrame {
 
     /**
