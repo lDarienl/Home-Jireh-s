9caa99fa9716dd673fca379c996ba0e4cc7dddea
Moonspot011 <145489380+Moonspot011@users.noreply.github.com>
2024-06-11 01:48:32 -0500

    Update ClienteControlador.java

--- Home_Jirehs/src/main/java/controlador/ClienteControlador.java
+++ Home_Jirehs/src/main/java/controlador/ClienteControlador.java
@@ -16,11 +16,45 @@ import vista.*;
 public class ClienteControlador implements ActionListener{
     private DtoCliente cliente;
     private Cliente_Detalle UICliente;
+    private panel_control_admin UIAdmin;
+    private panel_control_tele UITele;
+    private panel_control_vendedor UIVendedor;
+    private Agregar_Cliente UIAgregarCliente;
     private Cliente modelo;
 
+    public ClienteControlador(Agregar_Cliente UIAgregarCliente) {
+        this.UIAgregarCliente = UIAgregarCliente;
+        modelo = new Cliente();
+        this.UIAgregarCliente.jButton1.addActionListener(this);
+    }
+
+    
+    
+    public ClienteControlador(panel_control_admin UIAdmin) {
+        this.UIAdmin = UIAdmin;
+        this.UIAdmin.jButton3.addActionListener(this);
+        modelo = new Cliente();
+    }
+
+    public ClienteControlador(panel_control_tele UITele) {
+        this.UITele = UITele;
+        this.UITele.jButton3.addActionListener(this);
+        modelo = new Cliente();
+    }
+
+    public ClienteControlador(panel_control_vendedor UIVendedor) {
+        this.UIVendedor = UIVendedor;
+        this.UIVendedor.jButton3.addActionListener(this);
+        modelo = new Cliente();
+    }
+    
+    
+    
+    
     public ClienteControlador(Cliente_Detalle UICliente) {
         this.UICliente = UICliente;
         modelo = new Cliente();
+        mostar();
         UICliente.botonCerrar.addActionListener(this);
     }
     //metodo para mostar los datos de un cliente
@@ -44,6 +78,22 @@ public class ClienteControlador implements ActionListener{
         if(UICliente.botonCerrar.equals(e.getSource())){
             UICliente.setVisible(false);
         }
+        if(UIAdmin.jButton3.equals(e.getSource())){
+            UIAgregarCliente.setVisible(true);
+        }
+        if(UIAgregarCliente.jButton1.equals(e.getSource())){
+            modelo.crearCliente(Integer.valueOf(UIAgregarCliente.textID.getText()), UIAgregarCliente.textNombre.getText(), UIAgregarCliente.textEmail.getText(), Integer.valueOf(UIAgregarCliente.textTelefono.getText()), UIAgregarCliente.textDireccion.getText()); 
+        }
+        if(UIAgregarCliente.botonCerrar.equals(e.getSource())){
+            UIAgregarCliente.setVisible(false);
+        }
+        if(UITele.jButton3.equals(e.getSource())){
+            UIAgregarCliente.setVisible(true);
+        }
+        if(UIVendedor.jButton3.equals(e.getSource())){
+            UIAgregarCliente.setVisible(true);
+        }
     }
     
 }
+
